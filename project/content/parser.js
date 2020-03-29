8005a8a3aeaddf84368023d972937a6e66d6d7e7
Landon Harris <lharri73@vols.utk.edu>
2020-03-28 23:17:20 -0400

    reading the DOM for username and password values
    - this is kind of a brute force way to do it, but it works
    - searches for form tag, then searches for first password, then text
    field before for username
    - updated timelog

--- project/content/parser.js
+++ project/content/parser.js
@@ -1,22 +1,49 @@
 class FormReader {
 	getFormFields(form){
-		let pwFields = [];
-		let userFields = [];
+		let passwordIndex = -1;
+		let usernameIndex = -1;
 
 		// search the DOM for any form fields we might need
 		for (let i = 0; i < form.elements.length; i++){
-			if(form.elements[i].localName.toLowerCase() != "input" &&
-				(form.elements[i].type == undefined || form.elements[i].type == null))
-				continue; // it's not a username or password field
+			if(!this.isInputType(form.elements[i])) continue;
+			
+			const DOMtype = form.elements[i].type.toLowerCase();
+			const value = form.elements[i].value;
+
+			console.log("processing field with domtype", DOMtype, value);
+			
+			if(DOMtype == "password"){
+				passwordIndex = i;
+				this.passwordField = form.elements[i];
+				break;
+			}
+		}
+		for(let i = passwordIndex; i >= 0; i--){
+			if(!this.isInputType(form.elements[i])) continue;
 
 			const DOMtype = form.elements[i].type.toLowerCase();
+			const value = form.elements[i].value;
 
-			if(DOMtype == "fieldset")
-				continue; // probably not a fieldset
-
-			if(DOMtype == "radio") continue;
-			if(DOMtype == "select-one") continue;
-			console.log("processing field with domtype", DOMtype);
+			console.log("processing field with domtype", DOMtype, value);
+			if(DOMtype == "text"){
+				usernameIndex = i;
+				this.usernameField = form.elements[i];
+				break;
+			}
 		}
+		console.log("username:", this.usernameField);
+		console.log("password:", this.passwordField);
+	}
+
+
+	isInputType(element){
+		if(element.localName.toLowerCase() != "input" &&
+			(element.type == undefined || element.type == null))
+			return false; // not a username or password field
+		if(element.type.toLowerCase() == "fieldset")
+			return false; // probably not a fieldset
+		if(["radio", "select-one", "checkbox", "submit"].includes(element.type.toLowerCase()))
+			return false;
+		return true;
 	}
 }
