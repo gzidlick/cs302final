14e00bbbe94556c6d48338048f49e22058c1fefa
Landon Harris <lharri73@vols.utk.edu>
2020-04-08 00:28:36 -0400

    working on submit handler

--- project/content/parser.js
+++ project/content/parser.js
@@ -36,6 +36,82 @@ class FormReader {
 	}
 
 
+	registerSubmitHandler(form){
+		const button = getSubmitButton(form);
+		const handler = e => this.submit(e,form);
+		button.addEventListener("click", handler);
+		form.addEventListener("submit", handler);
+	}
+
+	getSubmitButton(form){
+		const possibleButtons = [];
+		Array.from(form.ownerDocument.getElementsByTagName("button").forEach(value => {
+			if(!value.isConnected) return;
+			if(!value.type || value.type != "reset"){
+				const possibleNames = [];
+				if (value.name) possibleNames.push(value.name.toLowerCase());
+				if (value.textContent) possibleNames.push(value.textContent.toLowerCase());
+				if (value.value) possibleNames.push(value.value.toLowerCase());
+				const button_score = scoreButton(possibleNames);
+				
+				if(button_score == -1) continue;
+				if(button_score ==  0)	// save it for later
+					possibleButtons.push(value);
+
+				if(button_score == 1){
+					//The button!
+					return value
+				}
+			}
+		}
+		return possibleButtons[0];	// shouldn't get here but this is just in case
+	}
+
+
+	scoreButton(possibleNames){
+		const goodWords = ["submit", "login", "enter", "log in", "signin",
+            "sign in", "next", "continue"];
+        const badWords = ["reset", "cancel", "back", "abort", "undo", "exit",
+            "empty", "clear", "captcha", "totp", "forgot", "dismiss", "delete",
+            "show", "reveal"];
+
+		let good = false;
+		let bad = false;
+
+		for(let i=0; i < possibleNames.length; i++){
+			if(badScore) break;
+			const curVal = possibleNames[i].trim(); // remove whitespace
+			if(!curVal) continue;					// don't care if it's empty
+
+			for(let j=0; j < badWords.length; j++){
+				if(curVal.indexOf(badWords[j]) >=){
+					badScore = true;
+					break;
+				}
+			}
+		}
+
+		for(let i=0; i < possibleNames.length; i++){
+			if(goodScore) break;
+			const curVal = possibleNames[i].trim(); // remove whitespace
+			if(!curVal) continue;					// don't care if it's empty
+
+			for(let j=0; j < goodWords.length; j++){
+				if(curVal.indexOf(goodWords[j]) >=){
+					goodScore = true;
+					break;
+				}
+			}
+		}
+
+		if(goodScore && badScore) return 0;
+		if(badScore) return -1;
+		if(goodScore) return 1;
+		return 0;	// !goodScore && !badScore
+	}
+
+	
+	// Helper function to find username and password fields
 	isInputType(element){
 		if(element.localName.toLowerCase() != "input" &&
 			(element.type == undefined || element.type == null))
